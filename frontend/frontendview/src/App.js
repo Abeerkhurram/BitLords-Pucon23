3213ea1533496f8d4468e5527469bcf3d8b64baa
professionalbot <110720852+professionalbot@users.noreply.github.com>
2023-06-03 13:15:58 +0500

    registration Page

---
+++ frontend/frontendview/src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Register from './component/Registration/register.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <Register/>
+    </div>
+  )
+}
