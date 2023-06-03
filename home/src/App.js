79de6d161894d2da8b7b02f5efbe0e8715b86a48
Abeer.K <91427896+Abeerkhurram@users.noreply.github.com>
2023-06-03 12:05:55 +0500

    navbar

---
+++ home/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/navbar.js";
+function App() {
+  return (
+    <>
+    
+      <Navbar/>
+    </>
+  );
+}
+
+export default App;
